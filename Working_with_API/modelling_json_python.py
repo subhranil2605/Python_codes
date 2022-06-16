5787ea70d431be644638a2156915afdc45028830
Subhranil Sarkar <57196419+subhranil2605@users.noreply.github.com>
2022-06-16 18:55:06 +0530

    Update modelling_json_python.py

--- Working_with_API/modelling_json_python.py
+++ Working_with_API/modelling_json_python.py
@@ -1,33 +1,36 @@
+from __future__ import annotations
 import json
+from typing import List, Dict, Any
+
 
 
 # Student class
 class Student:
-    def __init__(self, id_, name, phone):
-        self.id_ = id_
-        self.name = name
-        self.phone = phone
+    def __init__(self, id_: int, name: str, phone: str):
+        self.id_: int = id_
+        self.name: str = name
+        self.phone: str = phone
 
 
     # convert a string that generated from json to a Student instance 
     @staticmethod
-    def from_json_to_class(json):
+    def from_json_to_class(json: str) -> Student:
         return Student(json["id"], json["name"], json["phone_number"])
 
 
     # convert a Student instance into a python dictionary that can be used in JSON
-    def from_student_to_json(self):
+    def from_student_to_json(self: Student) -> Dict[str, Any]:
         return {"id": self.id_, "name": self.name, "phone_number": self.phone}
 
 
 
 # returns a list of Student instance
-def student_from_json(s: str):
+def student_from_json(s: str) -> List[Student]:
     return list(map(Student.from_json_to_class, s))
     
 
 # returns a list of dictionary
-def student_to_json(s):
+def student_to_json(s: List[Student]) -> List[Dict[str, Any]]:
     return list(map(Student.from_student_to_json, s))
 
 
@@ -58,22 +61,12 @@ So, it simulates a data created by API
 
 # reading the json file in python and stored the data
 with open("data.json", 'r') as f:
-    data = json.load(f)
+    data: list = json.load(f)
 
 
 
-student_class_list = student_from_json(data)
+student_class_list:  List[Student] = student_from_json(data)
 
-student_json = student_to_json(student_class_list)
+student_json: List[Dict[str, Any]] = student_to_json(student_class_list)
 
 print(student_json)
-
-
-
-
-
-
-
-
-    
-
